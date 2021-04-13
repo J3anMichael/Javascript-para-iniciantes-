c91f0a3e3431d2e9b2993ef93b2bc73d9e04a579
Jean Michael <73749746+J3anMichael@users.noreply.github.com>
2021-04-13 10:22:28 -0300

    Update exemplo.js

--- Arrow Function/exemplo.js
+++ Arrow Function/exemplo.js
@@ -1,8 +1,21 @@
-// A criação de uma arrow function consistem em 3 “passos”:
+// Feature 
+
+
+// A criação de uma arrow functions consistem em alguns “passos”:
 
 // Os parênteses, que é por onde a função recebe os argumentos (assim como na function tradicional);
 // A “seta” => - responsável pelo nome “arrow” function;
 // E as chaves: o bloco de código que representa o corpo da função.
+
+
+1- Var, Let ou Const
+2- Nome da variável 
+3- =
+4- (argumentos)
+5- =>
+6- {} 
+
+
 
 
 // Função normal
@@ -20,3 +33,21 @@ var mult = function(a, b){
 var mult3 = a => {
     console.log(a*2);
 }
+
+
+const mult4 = (a,b) => {
+    return 5 * 15;
+};
+
+
+
+
+// Forma Implícita
+
+const person = () => ({
+    return {
+        name: "Jean Michael",
+        lastName: "Claudino",
+        age: 24,
+        college: "Estacio"
+    });
