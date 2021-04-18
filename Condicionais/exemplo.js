d70c2fdcac1209e9ad579193efade582efc2e905
Jean Michael <73749746+J3anMichael@users.noreply.github.com>
2021-04-18 13:05:22 -0300

    Add files via upload

---
+++ Condicionais/exemplo.js
@@ -0,0 +1,59 @@
+// Estruturas condicionais Javascript são como estruturas condicionais na maioria das linguagens de programação existentes, as utilizamos para verificar uma condição e definir se algo deve ou não acontecer.
+// if = SESIM
+// else = SENAO
+
+// Exemplo:
+
+if (condicao) {
+    // codigo para executar caso a condição seja verdadeira
+  } else {
+    // senão, executar este código
+  }
+
+
+// // Variável booleana
+
+const verao = false; 
+
+  if(verao) {
+      console.log("Vamos para praia oh oh ohuh");
+  } else {
+      console.log("Vamos ficar em casa :c")
+  }
+
+  const verao = true; 
+
+  if(verao) {
+      console.log("Vamos para praia oh oh ohuh");
+  } else {
+      console.log("Vamos ficar em casa :c")
+  }
+
+
+  //////////////////////////////////////////////////////
+
+  var semafaro = "verde";
+
+  if(semafaro ==verde) {
+      console.log("pode passar")
+  } else {
+    console.log("pare!");
+}
+
+  var semafaro = "vermelho";
+
+  if(semafaro == "verde"){
+      console.log("pode passar");
+  } else {
+      console.log("pare!");
+  }
+
+//////////////////////////////////////////////////////////
+
+const Mask = 'The Mask';
+
+if (Mask === 'The Mask') {
+  console.log("Someeeebody stooop me")
+} else {
+  console.log("Did you miss me?.......I guess not")
+}
