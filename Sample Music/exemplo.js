ffaef1e0b59cc2a548bebe2ed8b214b506bb7155
Jean Michael <73749746+J3anMichael@users.noreply.github.com>
2021-04-19 17:32:24 -0300

    Add files via upload

---
+++ Sample Music/exemplo.js
@@ -0,0 +1,25 @@
+
+
+<body>
+
+
+<label>Name</label>
+<br>
+<br>
+<input type="text" placeholder="Write here" id="texto">
+<button onclick="myFunction()" >
+Click Here
+</button>
+
+
+<script>
+
+function myFunction() {
+var text = document.querySelector("#texto").value;
+window.alert("Welcome")
+};
+
+</script>
+
+</body>
+
