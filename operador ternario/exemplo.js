c45373812d9099e8044aba62608a5c5f8b87e1a8
Jean Michael <73749746+J3anMichael@users.noreply.github.com>
2021-04-25 11:20:00 -0300

    Add files via upload

---
+++ operador ternario/exemplo.js
@@ -0,0 +1,31 @@
+// O operador condicional ternário é um atalho para o condicional if.
+// O operador condicional é o único operador ternário de JavaScript.
+
+condition ? expr1 : expr2
+
+// condicao é uma expressão que é avaliada como true ou false.
+// expr1, expr2 são expressões com valores de qualquer tipo.
+// ? = if e : = else.
+
+// Exemplo:
+
+// Normal
+
+var livro = 50;
+
+if(livro < 40){
+    console.log("Livro barato")
+} else {
+    console.log("Livro caro")
+}
+
+
+// Ternário
+
+var preco = 50;
+let resultado = preco < 40 ? "livro barato" : "livro nao barato!";
+console.log(resultado)
+
+
+
+
