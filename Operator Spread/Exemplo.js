225e8ecc3dd2ef50959afe6cd019ff6963ac535f
Jean Michael <73749746+J3anMichael@users.noreply.github.com>
2021-04-27 09:48:09 -0300

    Update Exemplo.js

--- Operator Spread/Exemplo.js
+++ Operator Spread/Exemplo.js
@@ -32,3 +32,28 @@ var array = [1,2,3,4,...arr];
 console.log(array);
 
 // output: 1,2,3,4,5,6
+
+
+
+// ================================================
+
+
+var Livros1 = ["Javascript", "PHP", "React"];
+var Livros2 = ["Java", "Node", "C#"];
+var Preco = [100, 50, 85, 125, 70, 95]
+var Livros = [...Livros1, ...Livros2, ...Preco];
+console.log(Livros)
+
+// output: ["Javascript", "PHP", "React", "Java", "Node", "C#", 100, 50, 85, 125, 70, 95]
+
+
+
+
+var Livros1 = ["Javascript", "PHP", "React"];
+var Livros2 = ["Java", "Node", "C#"];
+var Preco = [100, 50, 85, 125, 70, 95]
+var Livros = [...Livros1, ...Livros2, ...Preco];
+var precoMaximo = Math.max(...Preco);
+console.log(precoMaximo); // output: 125
+console.log(Livros)
+
