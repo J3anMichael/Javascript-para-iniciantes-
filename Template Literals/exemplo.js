3953f69487191ae3cec5abac7b824fb5ace5108a
Jean Michael <73749746+J3anMichael@users.noreply.github.com>
2021-05-02 17:23:18 -0300

    Update exemplo.js

--- Template Literals/exemplo.js
+++ Template Literals/exemplo.js
@@ -1,4 +1,11 @@
-// Nada mais nada menos que uma forma mais facil de concatenar 
+// Template Literals e uma funcionalidade criada pela ES6 com finalidade de incrementar e 
+// ampliar as funcionalidades de Template Strings que existia nas versoes anteriores da ES.
+
+
+const nome = "Jean";
+const cidade = "Curitiba"
+
+console.log(`Boa tade, me chamo ${nome}, acabei de chegar de ${cidade}`);
 
 
 var nome = "Jean Michael";
@@ -10,7 +17,8 @@ console.log(frase);
 
 
 
-////////////////////////////////////////////////////
+// =============================================================
+
 
 var nome = "Jean Michael";
 var sobre = "Estudante de Sistemas de informacao";
