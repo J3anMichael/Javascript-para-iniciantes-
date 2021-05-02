d84f8eb0784d4c982a699981dd1c808e6cc0d315
Jean Michael <73749746+J3anMichael@users.noreply.github.com>
2021-05-02 17:21:43 -0300

    Add files via upload

---
+++ TemplateLiterals/exemplo.js
@@ -0,0 +1,8 @@
+// Template Literals e uma funcionalidade criada pela ES6 com finalidade de incrementar e 
+// ampliar as funcionalidades de Template Strings que existia nas versoes anteriores da ES.
+
+
+const nome = "Jean";
+const cidade = "Curitiba"
+
+console.log(`Boa tade, me chamo ${nome}, acabei de chegar de ${cidade}`);
