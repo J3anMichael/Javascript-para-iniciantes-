a9551ba51e6af85c66dbeced0fc2ba3767134a42
Jean Michael <73749746+J3anMichael@users.noreply.github.com>
2021-05-11 17:55:54 -0300

    Update exemplo.js

--- This/exemplo.js
+++ This/exemplo.js
@@ -1,1 +1,19 @@
-x
+
+
+
+
+
+
+
+
+
+
+
+const pessoa = {
+firstName: "Jean",
+lastName: "Michael",
+age: 24,
+fullDetails: function(){
+  console.log(this.firstName + ' ' + this.lastName + this.age);
+   }
+}
