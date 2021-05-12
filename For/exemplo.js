02b9fbe48ff6a05f05e91da527cacc0a7ef5e9ec
Jean Michael <73749746+J3anMichael@users.noreply.github.com>
2021-05-12 12:14:16 -0300

    Update exemplo.js

--- For/exemplo.js
+++ For/exemplo.js
@@ -1,1 +1,9 @@
-x
+for(let i = 0;
+     i < 5; i++) {
+    console.log("Working!",i)
+}
+// O for vai analisar o i, e i e menor que ZERO? Resposta: Sim, i e menor que ZERO
+// Entao i vai ser executado uma vez
+// o valor de ZERO atribuido a i, corresponde ao valor inicial 
+// i++ vai encrimentar por 1, e sempre que i for menor que 5, sera encrementado 1 no valor,
+// no nosso caso queremos 5 repeticoes.
