ff2aebf77d4546d4150c64141436e714661293b8
Jean Michael <73749746+J3anMichael@users.noreply.github.com>
2021-05-12 10:46:14 -0300

    Update exemplo.js

--- This/exemplo.js
+++ This/exemplo.js
@@ -1,4 +1,7 @@
 
+// this aponta para o contexto onde uma função está sendo executada. Seu valor dentro de uma função
+// depende de como a função foi invocada. Em uma mesma função, this pode ter diferentes valores, em
+// diferentes momentos, dependendo da invocação.
 
 
 
@@ -8,12 +11,26 @@
 
 
 
+let person = {
+    name: "Jean",
+    age: 24,
+    speach: function(){
+        console.log("Hello World!") 
+    },
+    sayName: function(){ // metodo 
+        console.log("My name is " + this.name);
+    },
+    birth: function(){ // metodo fazer aniversario 
+        this.age += 1; // o this vai acessar o valor ja atribuido a age e em seguida vai atribuir o novo valor
+    }
+};
 
-const pessoa = {
-firstName: "Jean",
-lastName: "Michael",
-age: 24,
-fullDetails: function(){
-  console.log(this.firstName + ' ' + this.lastName + this.age);
-   }
-}
+person.speach();
+
+person.sayName();
+
+console.log(person.age);
+
+person.birth();
+
+console.log(person.age);
