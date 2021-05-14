a09ff1869280e4ec211ef6aaea1e495fe0476a10
Jean Michael <73749746+J3anMichael@users.noreply.github.com>
2021-05-14 13:11:45 -0300

    Add files via upload

---
+++ While/exemplo.js
@@ -0,0 +1,8 @@
+// WHILE
+
+let salario = 1500  // valor do nosso salario
+
+while (salario < 2500){  // Enquanto o salario for menor que 2500, acrescentar 250 no valor até chegar a 2500
+salario += 250;
+  console.log(salario)
+} 
