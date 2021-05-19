7e481b4ee10345d8bcca04b9df9e72186e697f41
Jean Michael <73749746+J3anMichael@users.noreply.github.com>
2021-05-19 10:56:32 -0300

    Update exemplo.js

--- POO/Extends/exemplo.js
+++ POO/Extends/exemplo.js
@@ -13,8 +13,8 @@ var r1 = new Retangulo(3, 4);
 var r2 = new Retangulo(3, 8);
 
 class Quadrado extends Retangulo {
-    constructor(dimensao) {
-      super(dimensao, dimensao)
+    constructor(dimensao) {   // constructor e uma sub-Classe
+      super(dimensao, dimensao) // chamamos o construtor da nossa super classe, passando os parametros que ele precisa.
     }
 }
 
