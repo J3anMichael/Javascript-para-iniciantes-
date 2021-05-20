73a5ffb21c446d96246391f4c4db9e354357a114
Jean Michael <73749746+J3anMichael@users.noreply.github.com>
2021-05-20 13:54:24 -0300

    Add files via upload

---
+++ Module/modulo1.js
@@ -0,0 +1,10 @@
+function soma(a, b){
+    return a + b;
+}
+
+function subtrai(a, b){
+    return a - b;
+}
+
+
+export { soma, subtrai };
---
+++ Module/modulo2.js
@@ -0,0 +1,4 @@
+import { soma, subtrai } from "./modulo1.js";
+
+console.log(soma(2, 2));
+console.log(subtrai(5, 6));
