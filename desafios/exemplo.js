680927b43c13d959e6277a4bcbb3a0869fd0d135
Jean Michael <73749746+J3anMichael@users.noreply.github.com>
2021-08-06 20:57:50 -0300

    Update exemplo.js

--- desafios/exemplo.js
+++ desafios/exemplo.js
@@ -1,1 +1,12 @@
-v
+// Desafios
+
+
+// Desafio - 1
+
+// Escreva uma função que converte Horas em Segundos:
+
+function ConverterHoras(horas){   // horas = número em horas
+return horas * 60 * 60            //  1 hora tem 60 minutos e 1 minuto tem 60 segundos 
+}
+
+console.log(ConverterHoras(1)) //3600
