8d7dd95c86f7ad45a83a8404ae2984f63fa1839c
Jean Michael <73749746+J3anMichael@users.noreply.github.com>
2021-08-07 11:03:21 -0300

    Update exemplo.js

--- desafios/exemplo.js
+++ desafios/exemplo.js
@@ -5,11 +5,11 @@
 
 // Escreva uma função que converte Horas em Segundos:
 
-function ConverterHoras(horas){   // horas = número em horas
+function HourToSeconds(hour){   // horas = número em horas
 return horas * 60 * 60            //  1 hora tem 60 minutos e 1 minuto tem 60 segundos 
 }
 
-console.log(ConverterHoras(1)) // 3600 segundos
+console.log(HourToSeconds(1)) // 3600 segundos
 
 
 // =========================
@@ -23,3 +23,16 @@ return time * 24
 }
 
 console.log(WeekToHours(7)) // 168 horas
+
+
+// =========================
+
+// Desafio - 3
+
+// Escreva uma função que converte Dias em Horas
+
+function DayToHours(day){
+return day * 24 
+}
+
+console.log(DayToHours(31))  // 744 horas
